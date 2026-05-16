6530f981636efb6c30e785f7d56a4674de4cf560
biancka <biancka30ar@gmail.com>
2026-05-16 09:22:00 -0600

    feat: setuo del proyecto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return <div><h1>Estampas del Mundial 2026</h1></div>
+}
+
+export default App
